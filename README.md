95032f9ee9614be64bc0ba9a594bd9b2563ad8ee
Sayem-Ahmed-Shayeed <pshayeed1@gmail.com>
2024-07-17 22:47:28 +0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,21 @@
+<!-- This chrome extension is help us a little bit.
+Sometimes we browse through some websites where we find some words or some language which is a little bit unfamiliar to us then we need to translate the language to our native understandable language. 
+but 
+1->opening a new tab.
+2->Select the text.
+3->Copy the text.
+4->Select the Language.
+5->Paste the text 
+then you will find the translate version.
+this is quite Numbersome job , Right?
+
+<!-- --> I was Also doing so and to solve this problem i came up with the extension.
+
+
+<!--->  How this works??
+
+1.Select the text.
+2.Right click on the mouse.
+3.Select translate Boss.
+
+
---
+++ content.js
@@ -0,0 +1,25 @@
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: "translateToBangla",
+    title: "Translate Boss : UwU",
+    contexts: ["selection"]
+  });
+});
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === "translateToBangla") {
+    const selectedText = info.selectionText;
+    if (selectedText) {
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: translateText,
+        args: [selectedText]
+      });
+    }
+  }
+});
+
+function translateText(selectedText) {
+  const url = `https://translate.google.com/?sl=en&tl=bn&text=${encodeURIComponent(selectedText)}&op=translate`;
+  window.open(url, '_blank');
+}
